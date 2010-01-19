d6d3cbecb9cba8f87ee292f659f4bfed0fb56cf2
Stephen Lewis <git@experienceinternet.co.uk>
2010-01-19 11:07:13 +0000

    Forgot to update version number, duh.

--- system/extensions/fieldtypes/bucketlist/ft.bucketlist.php
+++ system/extensions/fieldtypes/bucketlist/ft.bucketlist.php
@@ -9,7 +9,7 @@ if ( ! defined('EXT'))
  * Seamlessly integrate Amazon S3 with your ExpressionEngine website.
  *
  * @package   	BucketList
- * @version   	1.0.0
+ * @version   	1.0.1
  * @author    	Stephen Lewis <addons@eepro.co.uk>
  * @copyright 	Copyright (c) 2009, Stephen Lewis
  * @link      	http://eepro.co.uk/bucketlist/
@@ -27,7 +27,7 @@ class Bucketlist extends Fieldframe_Fieldtype {
 	 */
 	public $info = array(
 		'name'				=> 'BucketList',
-		'version'			=> '1.0.0',
+		'version'			=> '1.0.1',
 		'desc'				=> 'Seamlessly integrate Amazon S3 with your ExpressionEngine site.',
 		'docs_url'			=> 'http://eepro.co.uk/bucketlist/',
 		'versions_xml_url'	=> 'http://eepro.co.uk/addon-versions.xml'
