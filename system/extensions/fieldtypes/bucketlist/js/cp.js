e5c4b66e38ef6c16eefe6bf269941455bef2ea2b
Stephen Lewis <git@experienceinternet.co.uk>
2010-02-08 21:27:12 +0000

    Rearranged things to bypass problem with jQuery.noConflict() being used in other add-ons.

--- system/extensions/fieldtypes/bucketlist/js/cp.js
+++ system/extensions/fieldtypes/bucketlist/js/cp.js
@@ -7,153 +7,154 @@
  * @link 		http://eepro.co.uk/bucketlist/
  */
 
-// Is the downloads status bar visible?
-statusActive = false;
+jQuery.noConflict();
+jQuery(document).ready(function($) {
+	
+	// Is the downloads status bar visible?
+	statusActive = false;
 
-/**
- * Controls the display of the status bar.
- *
- * @access	private
- * @param 	bool	showStatus		Show or hide the status bar (default true).
- * @return 	void
- */
-function displayStatusBar(showStatus) {
-	
-	// Enough with the typing.
-	$statusBar = $('#bucketload-status');
-	
-	// Where are we heading to?
-	targetBottom = (showStatus === false) ? -$statusBar.outerHeight(true) * 1.5 : 0;
-	
-	// Animate!
-	$statusBar.stop(true, false).animate({bottom : targetBottom}, 500, function() {
-		statusActive = (showStatus !== false);
-	});
-}
+	/**
+	 * Controls the display of the status bar.
+	 *
+	 * @access	private
+	 * @param 	bool	showStatus		Show or hide the status bar (default true).
+	 * @return 	void
+	 */
+	function displayStatusBar(showStatus) {
+
+		// Enough with the typing.
+		$statusBar = $('#bucketload-status');
+
+		// Where are we heading to?
+		targetBottom = (showStatus === false) ? -$statusBar.outerHeight(true) * 1.5 : 0;
+
+		// Animate!
+		$statusBar.stop(true, false).animate({bottom : targetBottom}, 500, function() {
+			statusActive = (showStatus !== false);
+		});
+	}
 
 
-/**
- * Removes an upload item from the list.
- *
- * @access	private
- * @param 	string		uploadId		The upload ID of the item to remove.
- * @return 	void.
- */
-function removeUpload(uploadId) {
-	
-	// Shorthand.
-	$item = $('#bucketload-status-' + uploadId);
-	
-	// Hide the item.
-	$item.animate({width : 'hide', opacity : 0}, 500, function() {
-		// If this is the last item, hide the bar.
-		if ($(this).siblings().length == 0) {
-			displayStatusBar(false);
+	/**
+	 * Removes an upload item from the list.
+	 *
+	 * @access	private
+	 * @param 	string		uploadId		The upload ID of the item to remove.
+	 * @return 	void.
+	 */
+	function removeUpload(uploadId) {
+
+		// Shorthand.
+		$item = $('#bucketload-status-' + uploadId);
+
+		// Hide the item.
+		$item.animate({width : 'hide', opacity : 0}, 500, function() {
+			// If this is the last item, hide the bar.
+			if ($(this).siblings().length == 0) {
+				displayStatusBar(false);
+			}
+
+			// Remove this item.
+			$(this).remove();
+		});
+	}
+
+
+	/**
+	 * Notifies the user of a successful upload.
+	 *
+	 * @access	private
+	 * @param 	object		params		listItem, message, status, uploadId.
+	 */
+	function uploadSuccess(params) {
+		$('li#bucketload-status-' + params['uploadId'])
+			.removeClass('active')
+			.addClass('complete')
+			.html(params.message);
+
+		setTimeout('removeUpload("' + params.uploadId + '")', 1500);
+	}
+
+
+	/**
+	 * Notifies the user of a failed upload.
+	 *
+	 * @access	private
+	 * @param	object		params		listItem, message, status, uploadId.
+	 */
+	function uploadFailure(params) {
+		if (params.message == '' || params.message == 'undefined') {
+			params.message = languageStrings.uploadFailureGeneric;
 		}
-		
-		// Remove this item.
-		$(this).remove();
-	});
-}
+
+		$('#bucketload-status li#bucketload-status-' + params.uploadId)
+			.removeClass('active')
+			.addClass('error')
+			.html(params.message);
+	}
 
 
-/**
- * Notifies the user of a successful upload.
- *
- * @access	private
- * @param 	object		params		listItem, message, status, uploadId.
- */
-function uploadSuccess(params) {
-	$('li#bucketload-status-' + params['uploadId'])
-		.removeClass('active')
-		.addClass('complete')
-		.html(params.message);
-		
-	setTimeout('removeUpload("' + params.uploadId + '")', 1500);
-}
+	/**
+	 * Notifies the user that the upload has started, by adding an item
+	 * to the upload list.
+	 *
+	 * @access	private
+	 * @param	object		params		fileName, uploadId.
+	 */
+	function uploadStart(params) {
+		// Stupid IE has to be different, and include some nonsense path with the file name.
+		fileName = params.fileName.replace(/.*(\/|\\)/, '');
+
+		// Create the new item, append it to the list, and fade it in.
+		$li = $('<li class="active" id="bucketload-status-' + params.uploadId + '">' + fileName + '</li>');
+		$li.appendTo('#bucketload-status ul').hide().fadeIn('fast');
+
+		// If the status bar is currently hidden, show it.
+		if (statusActive == false) {
+			displayStatusBar(true);
+		}
+	}
 
 
-/**
- * Notifies the user of a failed upload.
- *
- * @access	private
- * @param	object		params		listItem, message, status, uploadId.
- */
-function uploadFailure(params) {
-	if (params.message == '' || params.message == 'undefined') {
-		params.message = languageStrings.uploadFailureGeneric;
+	/**
+	 * Alerts the user to in-progress uploads when he attempts to save, preview, or quick save
+	 * the page. If there are no in-progress uploads, we let him proceed without question.
+	 *
+	 * @access	public
+	 * @param 	object		e		jQuery event object.
+	 */
+	function handleSubmit(e) {
+		pendingUploads = $('#bucketload-status li').length;
+
+		if (typeof(pendingUploads) == 'number' && pendingUploads > 0) {
+			return confirm(languageStrings.confirmExit);
+		}
+	}
+
+
+	/**
+	 * Alerts the user to in-progress uploads when he attempts to navigate away from
+	 * the page, or close the window. If there are no in-progress uploads, we let him
+	 * proceed without question.
+	 *
+	 * @access	public
+	 * @param 	object		e		Native browser event object (event not bound using jQuery)
+	 */
+	function handleNavigate(e) {
+		pendingUploads = $('#bucketload-status li').length;
+
+		if (typeof(pendingUploads) == 'number' && pendingUploads > 0) {
+			return languageStrings.confirmExit;
+		}
 	}
 	
-	$('#bucketload-status li#bucketload-status-' + params.uploadId)
-		.removeClass('active')
-		.addClass('error')
-		.html(params.message);
-}
-
-
-/**
- * Notifies the user that the upload has started, by adding an item
- * to the upload list.
- *
- * @access	private
- * @param	object		params		fileName, uploadId.
- */
-function uploadStart(params) {
-	// Stupid IE has to be different, and include some nonsense path with the file name.
-	fileName = params.fileName.replace(/.*(\/|\\)/, '');
 	
-	// Create the new item, append it to the list, and fade it in.
-	$li = $('<li class="active" id="bucketload-status-' + params.uploadId + '">' + fileName + '</li>');
-	$li.appendTo('#bucketload-status ul').hide().fadeIn('fast');
 	
-	// If the status bar is currently hidden, show it.
-	if (statusActive == false) {
-		displayStatusBar(true);
-	}
-}
-
-
-/**
- * Alerts the user to in-progress uploads when he attempts to save, preview, or quick save
- * the page. If there are no in-progress uploads, we let him proceed without question.
- *
- * @access	public
- * @param 	object		e		jQuery event object.
- */
-function handleSubmit(e) {
-	pendingUploads = $('#bucketload-status li').length;
-	
-	if (typeof(pendingUploads) == 'number' && pendingUploads > 0) {
-		return confirm(languageStrings.confirmExit);
-	}
-}
-
-
-/**
- * Alerts the user to in-progress uploads when he attempts to navigate away from
- * the page, or close the window. If there are no in-progress uploads, we let him
- * proceed without question.
- *
- * @access	public
- * @param 	object		e		Native browser event object (event not bound using jQuery)
- */
-function handleNavigate(e) {
-	pendingUploads = $('#bucketload-status li').length;
-	
-	if (typeof(pendingUploads) == 'number' && pendingUploads > 0) {
-		return languageStrings.confirmExit;
-	}
-}
-
-
-
-/**
- * ------------------------------------------------------------------
- * Initialisation.
- * ------------------------------------------------------------------
- */
-
-$(document).ready(function() {
+	/**
+	 * ------------------------------------------------------------------
+	 * Initialisation.
+	 * ------------------------------------------------------------------
+	 */
 	
 	selector = 'input[type="submit"][name="preview"], input[type="submit"][name="save"], input[type="submit"][name="submit"]';
 	
