73376192741f486e37038e07e5fd3834a7028994
Stephen Lewis <git@experienceinternet.co.uk>
2010-03-02 23:30:32 +0000

    Added closing '</div>' to field settings method. Duh.

--- system/extensions/fieldtypes/bucketlist/ft.bucketlist.php
+++ system/extensions/fieldtypes/bucketlist/ft.bucketlist.php
@@ -9,7 +9,7 @@ if ( ! defined('EXT'))
  * Seamlessly integrate Amazon S3 with your ExpressionEngine website.
  *
  * @package   	BucketList
- * @version   	1.1.0
+ * @version   	1.1.1
  * @author    	Stephen Lewis <addons@experienceinternet.co.uk>
  * @copyright 	Copyright (c) 2009, Stephen Lewis
  * @link      	http://experienceinternet.co.uk/bucketlist/
@@ -27,7 +27,7 @@ class Bucketlist extends Fieldframe_Fieldtype {
 	 */
 	public $info = array(
 		'name'				=> 'BucketList',
-		'version'			=> '1.1.0',
+		'version'			=> '1.1.1',
 		'desc'				=> 'Seamlessly integrate Amazon S3 with your ExpressionEngine site.',
 		'docs_url'			=> 'http://experienceinternet.co.uk/bucketlist/',
 		'versions_xml_url'	=> 'http://experienceinternet.co.uk/addon-versions.xml'
@@ -1771,6 +1771,8 @@ _HTML_;
 					.'</label>';
 			}
 		}
+		
+		$html .= '</div>';
 		
 		return array('cell2' => $html);
 	}
